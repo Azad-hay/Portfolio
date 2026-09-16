42e17b20d2ce0d1b310f405e51f1486f3b13e9b8
Az <azud@hotmail.fr>
2026-09-16 15:50:10 +0200

    project 1

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -17,7 +17,8 @@ function Projects() {
               Projet personnel, mise en pratique de HTML et CSS avec plusieurs techniques de mise en pages et de paginations.
             </p>
             <a href="https://azad-hay.github.io/Kyoto_Project/#home" className="button project-card__link">Voir le projet</a>
-            <a href="https://github.com/Azad-hay/Kyoto_Project" >Voir le code</a>
+            <a href="https://github.com/Azad-hay/Kyoto_Project"
+            className="button project-card__link" >Voir le code</a>
           </section>
 
           <section className="project-card">
@@ -26,8 +27,8 @@ function Projects() {
             <p className="project-card__description">
               Mise en oeuvres des bonnes pratiques de SEO ainsi que correctif de quelques bug sur le dynamisme du site. Projet réalisé durant la formation Développeur Web  Openclassrooms.
             </p>
-            <a href="https://azad-hay.github.io/OP_SEO/" >Voir le projet</a>
-            <a href="https://github.com/Azad-hay/OP_SEO" >Voir le code</a>
+            <a href="https://azad-hay.github.io/OP_SEO/" className="button project-card__link">Voir le projet</a>
+            <a href="https://github.com/Azad-hay/OP_SEO" className="button project-card__link">Voir le code</a>
           </section>
 
           <section className="project-card">
@@ -36,8 +37,8 @@ function Projects() {
             <p className="project-card__description">
               Projet essentillement basé sur React et Vite. Utilisation des components, des props et du routage. Projet réalisé durant la formation Développeur Web  Openclassrooms.
             </p>
-            <a href="https://azad-hay.github.io/" >Voir le projet</a>
-            <a href="https://github.com/Azad-hay/React_project" >Voir le code</a>
+            <a href="https://azad-hay.github.io/" className="button project-card__link">Voir le projet</a>
+            <a href="https://github.com/Azad-hay/React_project" className="button project-card__link">Voir le code</a>
           </section>
         </div>
       </main>
